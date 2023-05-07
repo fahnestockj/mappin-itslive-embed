54bceff44c63b9597e8c7b3f6ddc09d0fc643301
Jacob Fahnestock <jrfz31@gmail.com>
2023-05-07 14:52:48 -0400

    fix: beep -> mappin-embed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,8 +9,8 @@ module.exports = {
   },
   plugins: [],
   
-  //NOTE: Scopes tailwind to only override classes in the project (root element id is 'beep')
-  important: '#beep',
+  //NOTE: Scopes tailwind to only override classes in the project (root element id is 'mappin-embed')
+  important: '#mappin-embed',
   
   //NOTE: prevents tailwind from injecting global styles
   corePlugins: { 
